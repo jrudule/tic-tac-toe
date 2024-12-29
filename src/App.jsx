0150df4ea4ec4e6e24b3c76214012ee1d46d3d3b
Justine <jrudule@gmail.com>
2024-12-29 02:51:21 +0200

    user symbol choice added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [isStarted, setIsStarted] = useState(false);
   const [board, setBoard] = useState(Array(3).fill(Array(3).fill('')));
   const [isEnded, setIsEnded] = useState(false);
   const [isWinner, setIsWinner] = useState(null);
+  const [symbol, setSymbol] = useState('X');
+  const [computerSymbol, setComputerSymbol] = useState('O');
+
+  function startGame() {
+    setIsStarted(true);
+    setComputerSymbol('O');
+
+    if (symbol === 'O') {
+      setComputerSymbol('X');
+      const emptyCells = [];
+      board.forEach((row, rIndex) => {
+        row.forEach((cell, cIndex) => {
+          if (!cell) emptyCells.push([rIndex, cIndex]);
+        });
+      });
+
+      const [compRow, compCol] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+      const computerBoard = board.map((row, rIndex) =>
+        row.map((cell, cIndex) => (rIndex === compRow && cIndex === compCol ? 'X' : cell))
+      );
+      setBoard(computerBoard);
+    }
+  }
 
   function checkWinner(board) {
     const size = board.length;
@@ -95,24 +119,22 @@ function App() {
   
     return null;
   }
-  
 
   function addSymbol(rowIndex, colIndex) {
     if(board[rowIndex][colIndex] !== '') return;
-    
+
     // User move
     const userBoard = board.map((row, rIndex) =>
-      row.map((cell, cIndex) => (rIndex === rowIndex && cIndex === colIndex ? 'X' : cell))
+      row.map((cell, cIndex) => (rIndex === rowIndex && cIndex === colIndex ? symbol : cell))
     );
     setBoard(userBoard);
   
     if (checkWinner(userBoard)) {
-      console.log('You won');
       setIsEnded(true);
       setIsWinner('user');
       return;
     }
-
+    
     const emptyCells = [];
     userBoard.forEach((row, rIndex) => {
       row.forEach((cell, cIndex) => {
@@ -121,26 +143,26 @@ function App() {
     });
 
     if (emptyCells.length === 0) {
-      setIsWinner("It's a tie");
+      setIsWinner('tie');
       setIsEnded(true);
       return;
     }
 
     // Computer's move
     let compRow, compCol, result;
-    if (findWinningRowOrCol(userBoard, 'O')) {
-      result =findWinningRowOrCol(userBoard, 'O');
+    if (findWinningRowOrCol(userBoard, computerSymbol)) {
+      result =findWinningRowOrCol(userBoard, computerSymbol);
     } 
-    else if (findWinningDiagonal(userBoard, 'O')) {
-      result = findWinningDiagonal(userBoard, 'O');
+    else if (findWinningDiagonal(userBoard, computerSymbol)) {
+      result = findWinningDiagonal(userBoard, computerSymbol);
     } 
-    else if (findWinningRowOrCol(userBoard, 'X')) {
-      result = findWinningRowOrCol(userBoard, 'X');
+    else if (findWinningRowOrCol(userBoard, symbol)) {
+      result = findWinningRowOrCol(userBoard, symbol);
     } 
     else {
-      result = findWinningDiagonal(userBoard, 'X');
+      result = findWinningDiagonal(userBoard, symbol);
     }
-    console.log(userBoard);
+
     if (result) {
       compRow = result.row;
       compCol = result.col;
@@ -148,31 +170,76 @@ function App() {
       [compRow, compCol] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
     }  
     const computerBoard = userBoard.map((row, rIndex) =>
-      row.map((cell, cIndex) => (rIndex === compRow && cIndex === compCol ? 'O' : cell))
+      row.map((cell, cIndex) => (rIndex === compRow && cIndex === compCol ? computerSymbol : cell))
     );
     setBoard(computerBoard);
 
     if (checkWinner(computerBoard)) {
-      console.log('You lost');
       setIsEnded(true);
       setIsWinner('computer');
     }
+
+    if (symbol === 'O' && emptyCells.length === 1) {
+      setIsWinner('tie');
+      setIsEnded(true);
+      return;
+    }
+  }
+  
+  function restartGame() {
+    setIsEnded(false);
+    setIsStarted(false);
+    setBoard(Array(3).fill(Array(3).fill('')));
   }
 
   return (
     <>
-      <div className={isEnded ? '' : 'hidden'}> {isWinner} </div>
-      {board.map((row, rowIndex) => (
-        <div key={rowIndex} className="row" style={{ display: "flex", marginBottom: "10px" }}>
-          {row.map((symbol, colIndex) => (
-            <div key={colIndex} className="col"
-              onClick={() => addSymbol(rowIndex, colIndex)}
-            >
-              {symbol}
-            </div>
-          ))}      
+      <div className={`${isStarted ? 'hidden' : ''} ${isEnded ? 'hidden' : ''}`}>
+        <h1>Tic Tac Toe</h1>
+        
+        <div className="XO">
+          <div>Choose between X and O</div>
+          <button className='X' onClick={() => setSymbol('X')}>
+             X
+          </button>
+          <button className='O' onClick={() => setSymbol('O')}>
+             O
+          </button>
         </div>
-      ))}
+        <button className='startButton' onClick={() => startGame()}>
+          Start
+        </button>
+      </div>
+
+      <div className={isEnded ? 'endGame' : 'hidden'}>
+          <div className='endGameText'>
+            {isWinner === 'computer' ? 'You lost!' : ''}
+            {isWinner === 'user' ? 'You won!' : ''}
+            {isWinner === 'tie' ? "It's a tie!" : ''}
+          </div>
+      </div>
+
+      <div className={isStarted ? '' : 'hidden'}>
+        <div>
+          {board.map((row, rowIndex) => (
+            <div key={rowIndex} className="row" style={{ display: "flex", marginBottom: "10px" }}>
+              {row.map((symbol, colIndex) => (
+                <div key={colIndex} className="col"
+                  onClick={() => addSymbol(rowIndex, colIndex)}
+                >
+                  {symbol}
+                </div>
+              ))}      
+            </div>
+          ))}
+        </div>
+
+        <div className={isEnded ? 'endGame' : 'hidden'}>
+          <button className='restartButton' onClick={() => restartGame()}>
+            Restart
+          </button>
+        </div>
+      </div>
     </>
   )
 }
